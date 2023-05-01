daeb2e8f4ed612e8e21589a942ad43e84ab97319
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-05-01 09:35:29 +0530

    ng rx installation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Page1Component,
-    Page2Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Page1Component, Page2Component],
+  imports: [BrowserModule, StoreModule.forRoot({})],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
